caa4a28da71b67972567c994a695222e2ef899ff
Diogo Monteiro <jdsasm@hotmail.com>
2017-03-18 21:50:08 +0000

    Now prompting user to make SMSecret the Default SMS app

--- app/src/main/java/com/example/spice/smsecret/MainMenuActivity.java
+++ app/src/main/java/com/example/spice/smsecret/MainMenuActivity.java
@@ -3,6 +3,7 @@ package com.example.spice.smsecret;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Telephony;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -22,6 +23,14 @@ public class MainMenuActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        //Prompting user to change to default SMS app
+        Intent intent = new Intent(Telephony.Sms.Intents.ACTION_CHANGE_DEFAULT);
+        intent.putExtra(Telephony.Sms.Intents.EXTRA_PACKAGE_NAME, getApplicationContext().getPackageName());
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        getApplicationContext().startActivity(intent);
+
+
         ins = this;
         Button inboxButton = (Button) findViewById(R.id.buttonInbox);
         Button composeButton = (Button) findViewById(R.id.buttonCompose);
